df33b7844b4fa1f7c10e989e48585f82081cb0d7
Abiral Shrestha <abiralstha333@gmail.com>
2025-07-14 17:01:50 -0400

    Added generalforminfo component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import './App.css'
 import GeneralInfoForm from './components/GeneralInfoForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // App.jsx will own the state and then will pass props to the components
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [address, setAddress] = useState('')
+  console.log(name, email, address)
 
   return (
     <>
-    <GeneralInfoForm/>
+    <GeneralInfoForm 
+      name={name}
+      setName={setName}
+      email={email}
+      setEmail={setEmail}
+      address={address}
+      setAddress={setAddress}
+    />
     </>
   )
 }
--- src/components/GeneralInfoForm.jsx
+++ src/components/GeneralInfoForm.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 
-function GeneralInfoForm() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [address, setAddress] = useState('')
-
+function GeneralInfoForm({name,setName, email, setEmail, address, setAddress}) {
     return (
-        <div>
-            <h1>CV Builder</h1>
-            
+        <div className="flex flex-col gap-4">
+            <input 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+                className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Name"
+            />
+            <input 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Email"
+            />
+            <input 
+                value={address} 
+                onChange={(e) => setAddress(e.target.value)} 
+                className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Address"
+            />
         </div>
     )
 }
