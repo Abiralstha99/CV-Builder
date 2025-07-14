943ed372968878782cf28c231f4e7c29fb8e6b6f
Abiral Shrestha <abiralstha333@gmail.com>
2025-07-14 18:07:16 -0400

    Improved UI for form and preview

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,28 @@ function App() {
   console.log(name, email, address)
 
   return (
-    <>
-    <GeneralInfoForm 
-      name={name}
-      setName={setName}
-      email={email}
-      setEmail={setEmail}
-      address={address}
-      setAddress={setAddress}
-    />
-    <ResumePreview 
-      name={name}
-      setName={setName}
-      email={email}
-      setEmail={setEmail}
-      address={address}
-      setAddress={setAddress}
-    />
-    </>
+    <div className='min-h-screen w-screen p-4 sm:p-6 bg-white text-black grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6'>
+      <div className='w-full'>
+        <GeneralInfoForm
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          address={address}
+          setAddress={setAddress}
+        />
+      </div>
+      <div className='w-full'>
+        <ResumePreview
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          address={address}
+          setAddress={setAddress}
+        />
+      </div>
+    </div>
   )
 }
 
